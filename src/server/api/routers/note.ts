2e4141a39c9dfb34e259f8e63f411b05f0e191a5
Sudip Kundu <sudipkundu999@gmail.com>
2023-03-20 13:42:43 +0530

    add topic & note router

---
+++ src/server/api/routers/note.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+
+export const noteRouter = createTRPCRouter({
+  getAll: protectedProcedure
+    .input(z.object({ topicId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.note.findMany({
+        where: { topicId: input.topicId },
+      });
+    }),
+  create: protectedProcedure
+    .input(
+      z.object({ title: z.string(), content: z.string(), topicId: z.string() })
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.note.create({
+        data: {
+          title: input.title,
+          content: input.content,
+          topicId: input.topicId,
+        },
+      });
+    }),
+  delete: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.note.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
