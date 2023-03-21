75384ff479e7267242372996cbe8cca464c33f79
Sudip Kundu <sudipkundu999@gmail.com>
2023-03-22 02:39:20 +0530

    max-width on desktop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-start p-5">
+      <main className="m-auto flex min-h-screen max-w-3xl flex-col items-center justify-start p-5">
         <Header />
       </main>
     </>
