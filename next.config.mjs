eb160b647115edcc52d8af78b8a1b0c2d052c969
Sudip Kundu <sudipkundu999@gmail.com>
2023-08-07 00:32:50 +0530

    [feat] docker-compose

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  output: "standalone",
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
